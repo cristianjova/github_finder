a5f01aa6bbaf108047a174c6aa892415590538db
Cristian Jovanovich <cristian.jova149@gmail.com>
2019-07-13 22:04:53 -0300

    fetch display repos,add showrepos

--- github.js
+++ github.js
@@ -2,16 +2,22 @@ class Github {
   constructor() {
     this.client_id = '2167d5aff4f2c2901096';
     this.client_secret = 'ecd599281ab5aa1f91e8b453f6ebe1f29e25ee9d';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&?client_secret=${this.client_secret}`);
 
+    const reposResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&?client_secret=${this.client_secret}`);
+
     const profile = await profileResponse.json();
+    const repos = await reposResponse.json();
 
     return {
       // is equal to profile: profile
-      profile
+      profile,
+      repos
     }
   }
 
--- ui.js
+++ ui.js
@@ -32,6 +32,32 @@ class UI {
     `;
   }
 
+  // Show user repos
+  showRepos(repos) {
+    let output = '';
+
+    repos.forEach((repo) => {
+      output += `
+        <div class="card card-body mb-2">
+          <div class="row"> 
+            <div class="col-md-6">
+              <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+            </div>
+
+            <div class="col-md-6">
+              <span class="badge badge-primary">Estrellas: ${repo.stargazers_count}</span>
+              <span class="badge badge-secondary">Visto: ${repo.watchers_count}</span>
+              <span class="badge badge-success">Forks: ${repo.forks_count}</span>
+            </div>
+          </div>
+        </div>
+      `;
+    });
+    
+    // Output the repos
+    document.getElementById('repos').innerHTML = output;
+  }
+
   // Show alert message
   showAlert(message, className) {
     //Clear any remaining alerts
